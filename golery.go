99467e84519d61f22dd0a39320611f7e571e3b92
andrefsp <andrefsp@gmail.com>
2015-12-23 17:09:15 +0000

    Add routing and error handling

--- golery.go
+++ golery.go
@@ -1,14 +1,15 @@
 package golery
 
 import (
-	"fmt"
 	"github.com/streadway/amqp"
+	"log"
 	"os"
 )
 
 type Route struct {
 	queueName string
 	fn        func([]byte)
+	workers   int
 }
 
 type Config struct {
@@ -32,33 +33,109 @@ func GetConfig(routes []Route) Config {
 	return Config{RABBITMQ_URL: RABBITMQ_URL, routeMap: routeMap}
 }
 
-func StartQueueConsumer(queueName string, config Config) {
+func createQueue(channel *amqp.Channel, queueName string) {
+
+	err := channel.ExchangeDeclare(
+		queueName, // name of the exchange
+		"direct",  // type
+		true,      // durable
+		false,     // delete when complete
+		false,     // internal
+		false,     // noWait
+		nil,       // arguments
+	)
+	if err != nil {
+		log.Fatal("Couldn't declare exchange: %s", err)
+	}
+
+	queue, err := channel.QueueDeclare(
+		queueName, // name of the queue
+		true,      // durable
+		false,     // delete when usused
+		false,     // exclusive
+		false,     // noWait
+		nil,       // arguments
+	)
+
+	if err != nil {
+		log.Fatal("Couldn't declare queue: %s", err)
+	}
+
+	err = channel.QueueBind(
+		queue.Name, // name of the queue
+		queueName,  // bindingKey
+		queueName,  // sourceExchange
+		false,      // noWait
+		nil,        // arguments
+	)
+	if err != nil {
+		log.Fatal("Couldn't bind queue: %s", err)
+	}
+
+}
+
+func messageConsumerWorker(messageChannel chan []byte, fn func([]byte)) {
+	defer func() {
+		if r := recover(); r != nil {
+			log.Println("Recover from", r)
+		}
+	}()
+
+	for message := range messageChannel {
+		fn(message)
+	}
+}
+
+func StartQueueConsumer(queueName string, config Config, TerminatedConsumerChannel chan<- bool) {
 	var connection, err = amqp.Dial(config.RABBITMQ_URL)
 
 	if err != nil {
-		fmt.Println("connection.open: %s", err)
+		log.Fatal("connection.open: %s", err)
 	}
 	defer connection.Close()
 
-	c, err := connection.Channel()
-
+	channel, err := connection.Channel()
 	if err != nil {
-		fmt.Println("channel.open: %s", err)
+		log.Fatal("channel.open: %s", err)
 	}
 
-	messages, err := c.Consume(queueName, queueName, true, false, false, false, nil)
+	createQueue(channel, queueName)
+
+	messages, err := channel.Consume(queueName, queueName, true, false, false, false, nil)
 	if err != nil {
-		fmt.Println("basic.consume: %v", err)
+		log.Fatal("basic.consume: %v", err)
 	}
 
+	var messageChannel = make(chan []byte)
+
+	var workers = 1
+
+	if config.routeMap[queueName].workers > 0 {
+		workers = config.routeMap[queueName].workers
+	}
+
+	// Start the workers
+	for i := 0; i < workers; i++ {
+		go messageConsumerWorker(messageChannel, config.routeMap[queueName].fn)
+	}
+
+	// Push messages into channel
 	for message := range messages {
-		config.routeMap[queueName].fn(message.Body)
+		messageChannel <- message.Body
 	}
+
+	TerminatedConsumerChannel <- true
 }
 
 func Start(routes []Route) {
 	var config = GetConfig(routes)
+	var TerminatedConsumerChannel = make(chan bool, len(routes))
+
 	for queueName, _ := range config.routeMap {
-		go StartQueueConsumer(queueName, config)
+		go StartQueueConsumer(queueName, config, TerminatedConsumerChannel)
+	}
+
+	for i := 0; i < len(routes); i++ {
+		<-TerminatedConsumerChannel
 	}
 }
--- golery_test.go
+++ golery_test.go
@@ -54,3 +54,30 @@ func TestCanIterateConfig(t *testing.T) {
 		t.Fail()
 	}
 }
+
+func TestCanPushMessages(t *testing.T) {
+	var receivedMessages = [][]byte{}
+	var function1 = func(message []byte) {
+		receivedMessages = append(receivedMessages, message)
+	}
+
+	var messageChannel = make(chan []byte)
+	go messageConsumerWorker(messageChannel, function1)
+
+	messageChannel <- []byte("This is a message")
+
+	if len(receivedMessages) < 1 {
+		t.Fail()
+	}
+}
+
+/*
+func TestItCanConsumeQueue(t *testing.T) {
+
+	Start([]Route{
+		Route{queueName: "go", fn: func(message []byte) {
+			fmt.Println(string(message))
+		}},
+	})
+}
+*/
